aaf2108e4d254ca66173cd15eb41f41008c14d5f
christine nguyen <cminhng@cmng-MacBook-Pro.local>
2024-11-14 09:53:37 -0800

    added basic functions to client and server

--- Client.java
+++ Client.java
@@ -1,3 +1,22 @@
+import java.net.Socket;
+
 public class Client {
-    
+    public Client(String name, int y){
+
+    }
+    public String request(String pw){
+        return "";
+        //i think this is where i do the factorization
+    }
+    public void handshake(){
+
+    }
+    public void disconnect(){
+
+    }
+    public Socket getSocket(){
+        Socket socket = new Socket();
+        return socket; 
+    }
+     
 }
--- Server.java
+++ Server.java
@@ -1,3 +1,15 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 public class Server {
-    
+    static ArrayList<LocalDateTime> connectedTimes;
+    public Server(int y){
+
+    }
+    public void serve(int numClients){
+        //connectedTimes.add();
+    }
+    public void disconnect(){
+        
+    }
 }
