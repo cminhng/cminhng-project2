17b1d6fd4fea4449411af9c5daf1139256271fcf
christine nguyen <cminhng@cmng-MacBook-Pro.local>
2024-11-16 21:44:20 -0800

    finished implementations of methods in client and server (sorry i forgot to commit

--- Client.java
+++ Client.java
@@ -1,21 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Client {
-    public Client(String name, int y){
+    private Socket socket;
+    private PrintWriter out;
+    private BufferedReader in;
 
+    public Client(String host, int port) throws IOException{
+        socket = new Socket(host, port);
+        out = new PrintWriter(socket.getOutputStream());
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
     }
-    public String request(String pw){
-        return "";
-        //i think this is where i do the factorization
+    public String request(String number) throws IOException{
+        out.println(number);
+        out.flush();
+        System.out.println("\nClient request: " + number);
+        String str = in.readLine();
+        System.out.println("\nServer reply: " + str);
+        return str;
     }
     public void handshake(){
-
+        //give pw
+        out.println("12345");
+        out.flush();
     }
+
     public void disconnect(){
-
+        try{
+            if(out != null){
+                out.close();
+            }
+            if(in != null){
+                in.close();
+            }
+            if(socket != null && !socket.isClosed()){
+                socket.close();
+            }
+        }catch(IOException e){
+            System.err.print("IOException");
+        }
     }
+
     public Socket getSocket(){
-        Socket socket = new Socket();
         return socket; 
     }
      
--- Server.java
+++ Server.java
@@ -1,15 +1,112 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Collections;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class Server {
-    static ArrayList<LocalDateTime> connectedTimes;
-    public Server(int y){
+    private static String pw = "12345";
+    private ServerSocket serverSocket;
+    private ArrayList<LocalDateTime> connectedTimes = new ArrayList<LocalDateTime>();
+    private int port;
 
+    public Server(int port) throws IOException{
+        this.port = port;
+        serverSocket = new ServerSocket(port);
     }
+
     public void serve(int numClients){
-        //connectedTimes.add();
+        for(int i = 0; i < numClients; i++){
+            try{
+                Socket clientSocket = serverSocket.accept();
+                connectedTimes.add(LocalDateTime.now());
+                //start thread
+                ClientHandler clientHandler = new ClientHandler(clientSocket);
+                clientHandler.start();
+            }catch(IOException e){
+                System.err.print("IOException");
+            }
+        }
     }
+
+    public static int factor(String number){
+        int numFactors = 0;
+        int n = Integer.parseInt(number);
+        for(int i = 1; i <= n; i++){
+            if(n % i == 0){
+                numFactors++;
+            }
+        }
+        return numFactors;
+    }
+
+    private static class ClientHandler extends Thread{
+        private Socket cSocket;
+        private PrintWriter out;
+        private BufferedReader in;
+        public ClientHandler(Socket clientSocket){
+            cSocket = clientSocket;
+        }
+
+        public void run(){
+            try{
+                out = new PrintWriter(cSocket.getOutputStream());
+                in = new BufferedReader(new InputStreamReader(cSocket.getInputStream()));
+
+                //handshake
+                if(!in.readLine().equals(pw)){
+                    out.println("couldn't handshake");
+                    //disconnect client
+                    cSocket.close();
+                    return;
+                }
+
+                //handling subsequent requests
+                while(true){
+                    String request = in.readLine();
+                    if(request == null || request.isEmpty()){
+                        break;
+                    }
+                    int numFactors = factor(request);
+                    out.println("The number " + request + " has " + numFactors + " factors");
+                }
+            }catch(IOException e){
+                System.err.print("IOException");
+            } finally {
+                try {
+                    if(out != null){
+                        out.close();
+                    }
+                    if(in != null){
+                        in.close();
+                    }
+                    if(cSocket != null){
+                        cSocket.close();
+                    }
+                } catch (IOException e) {
+                    System.err.print("IOException");
+                }
+            }
+        }
+    }
+
+    public ArrayList<LocalDateTime> getConnectedTimes(){
+        //sort connectedTimes;
+        Collections.sort(connectedTimes);
+        return connectedTimes;
+    }
+
     public void disconnect(){
-        
+        try {
+            if(serverSocket != null && !serverSocket.isClosed()){
+                serverSocket.close();
+            }
+        } catch (IOException e) {
+            System.err.println("IOException");
+        }
     }
 }
